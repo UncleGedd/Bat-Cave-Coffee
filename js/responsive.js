8e26799f05b0368f256eb96ca7ef057ef6240799
unknown <gedd@example.com>
2020-05-08 13:50:06 +0100

    adds small navbar

--- js/responsive.js
+++ js/responsive.js
@@ -1,15 +1,43 @@
 const adjustNav = function() {
-    console.log('hi')
+    MAX_WIDTH = 1040
     width = window.innerWidth;
     height = window.innerHeight;
-    MAX_WIDTH = 1000
     
-    if (width <= MAX_WIDTH && document.getElementsByTagName('nav').length != 0) {
+    if (width <= MAX_WIDTH && document.getElementsByTagName('nav').length > 0) {
         document.getElementsByTagName('nav')[0].remove()
-    } else {
+        document.getElementsByClassName('title-pics')[0].insertAdjacentElement('afterend', smallNavbar)
+    } else if (width > MAX_WIDTH && document.getElementsByTagName('nav').length == 0) {
         document.getElementsByClassName('subtitle')[0].insertAdjacentElement('afterend', navbar)
     }
 }
+
+const smallNavbar = document.createElement('div')
+smallNavbar.className = 'small-nav'
+smallNavbar.innerHTML = `
+    <ul class="nav-links">
+        <li class="nav-item nav-item-offset">
+            <a class="link-contents icon-link" href="https://www.liminicoffee.co.uk/">
+                <span class="nav-icon">menu</span>
+            </a>
+        </li>
+        <li class="nav-item nav-item-offset">
+            <a class="link-contents icon-link" href="https://www.liminicoffee.co.uk/">
+                <span class="nav-icon">account_circle</span>
+            </a>
+        </li>
+        <li class="nav-item nav-item-offset">
+            <a class="link-contents icon-link" href="https://www.liminicoffee.co.uk/">
+                <span class="nav-icon">shopping_cart</span>
+            </a>
+        </li>
+        <li class="nav-item nav-item-offset">
+            <a class="link-contents icon-link" href="https://www.liminicoffee.co.uk/">
+                <span class="nav-icon">shopping_basket</span>
+            </a>
+        </li>
+    </ul>
+`
+
 
 const navbar = document.getElementsByTagName('nav')[0]
 window.onload = adjustNav
